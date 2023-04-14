45b4727a05c58c249d7230f4a53c8668a5332310
Melvin121 <00016520@uca.edu.sv>
2023-04-14 13:01:53 -0600

    subiendo labo04

---
+++ Nueva carpeta/app/src/main/java/com/example/labo4/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.labo4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         private lateinit var button: Button
+         private lateinit var Name: EditText
+
+        override fun
+
+        //val inputText = phoneTextFIeld.editText?.text.toString()
+        //val inputname = nameTextFIeld.editText?.text.toString()
+        //val inputmail = mailTextField.editText?.text.toString()
+
+        //val intent = Intent(this, SecondActivity::class.java){
+            //intent.putExtra("key", "value",)
+            //startActivity(intent)
+        }
+
+        private fun click(){
+
+    }
+}
